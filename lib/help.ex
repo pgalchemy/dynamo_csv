87fbad096486219b42d1eb6f51b1ab9be4fe8108
Denys Medynskyi <denys.medynskyi@toptal.com>
2018-10-19 09:58:52 +0300

    Improve help output readability

--- lib/help.ex
+++ lib/help.ex
@@ -8,29 +8,31 @@ defmodule DynamoCSV.Help do
   def output do
     IO.puts("""
     -------------------------------------
+
     REQUIRED: You must pass --file option
 
     ./dynamo_csv --file /path/to/file.csv
-    -------------------------------------
 
     -------------------------------------
+
     OPTIONAL: You can pass --dryrun option.
     It will not submit data to Amazon.
     The output shows how many records would be processed
 
     /dynamo_csv --dryrun --file /path/to/file.csv
-    -------------------------------------
 
     -------------------------------------
+
     OPTIONAL: You can pass --table option to specify table name
 
     ./dynamo_csv --table my_table --file /path/to/file.csv
-    -------------------------------------
 
     -------------------------------------
+
     OPTIONAL: You can pass --columns to specify columns
 
     ./dynamo_csv --table my_table --columns first_name,last_name --file /path/to/file.csv
+
     -------------------------------------
     """)
   end
